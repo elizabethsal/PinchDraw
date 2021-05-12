5bdc310947d017a390d9faaef356a3b49e93b98e
DariaTarasova <dasharil1997@gmail.com>
2021-05-13 00:02:07 +0300

    test commit

--- app/src/main/java/com/example/pinchdraw/MainActivity.java
+++ app/src/main/java/com/example/pinchdraw/MainActivity.java
@@ -10,8 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
